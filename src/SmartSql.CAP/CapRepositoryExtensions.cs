1051e7e307106e4fdd9c86e94ad2ca6440c2a22f
xiangxiren <xiongliwei-2004@163.com>
2022-01-18 16:50:53 +0800

    AddCapRepository method adds parameters.

--- src/SmartSql.CAP/CapRepositoryExtensions.cs
+++ src/SmartSql.CAP/CapRepositoryExtensions.cs
@@ -1,14 +1,19 @@
-﻿using SmartSql.DIExtension;
+﻿using SmartSql;
+using SmartSql.DIExtension;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class CapRepositoryExtensions
     {
-        public static SmartSqlDIBuilder AddCapRepository(this SmartSqlDIBuilder builder)
+        public static SmartSqlDIBuilder AddCapRepository(this SmartSqlDIBuilder builder,
+            string alias = SmartSqlBuilder.DEFAULT_ALIAS)
         {
             builder.AddRepositoryFromAssembly(options =>
-                options.AssemblyString = typeof(CapRepositoryExtensions).Assembly.GetName().Name);
+            {
+                options.AssemblyString = typeof(CapRepositoryExtensions).Assembly.GetName().Name;
+                options.SmartSqlAlias = alias;
+            });
 
             return builder;
         }
